52ba4152ecb30ec42b9f023ce438f4fac8dbab73
jeanazurdia <65199229+jeanazurdia@users.noreply.github.com>
2020-05-11 16:08:03 -0700

    Create Rock-Paper-Scissors.py
    Basic rock-paper-scissors code.
    Using "else" statement to control all other inputs other than 'rock', 'paper', scissors

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,32 @@
+first = input('First move: ')
+second = input('Second move: ')
+
+if first == 'rock' and second == 'rock':
+	print('Tied game')
+
+elif first == 'rock' and second == 'paper':
+	print('second player wins')
+
+elif first == 'rock' and second == 'scissors':
+	print('first player wins')
+
+elif first == 'paper' and second == 'paper':
+	print('Tied game')
+
+elif first == 'paper' and second == 'scissors':
+	print('Second player wins')
+
+elif first == 'paper' and second == 'rock':
+	print('First player wins')
+
+elif first == 'scissors' and second == 'scissors':
+	print('Tied game')
+
+elif first == 'scissors' and second == 'paper':
+	print('First player wins')
+
+elif first == 'scissors' and second == 'rock':
+	print('Second player wins')
+
+else:
+	print('Incorrect input. Try again')
